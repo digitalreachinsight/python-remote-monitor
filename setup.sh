0a2d0107f6a5344bd3d08aeec1b6e52fe1fbf661
Jason Moore <jason@austwa.com>
2018-11-09 14:35:14 +0800

    Automatic Installer

--- setup.sh
+++ setup.sh
@@ -9,3 +9,4 @@ virtualenv -p python3 venv
 chmod +x /usr/lib/python-remote-monitor/update.sh
 groupadd drmonitor
 useradd -m drmonitor -g drmonitor -s /usr/sbin/nologin
+chown -R drmonitor.drmonitor /usr/lib/python-remote-monitor/
