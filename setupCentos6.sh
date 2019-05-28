f9f62bc04ef816b99183de4cf2c7e1df91520db8
Jason Moore <jason@austwa.com>
2019-05-28 16:41:09 +0800

    Centos Installer Updated

--- setupCentos6.sh
+++ setupCentos6.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-yum install python36 python-pip git -y
+yum install python34 python-pip git -y
 pip install -U pip
 pip install -U virtualenv
 cd /usr/lib/
 git clone https://github.com/digitalreachinsight/python-remote-monitor.git 
 cd /usr/lib/python-remote-monitor/
-virtualenv -p python36 venv
+virtualenv -p python34 venv
 /usr/lib/python-remote-monitor/venv/bin/pip3 install -r requirements.txt 
 chmod +x /usr/lib/python-remote-monitor/update.sh
 groupadd drmonitor
