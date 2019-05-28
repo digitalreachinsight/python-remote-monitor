e21799a2a8cc890b5872c703f7a4ff6b3dbee48c
Jason Moore <jason@austwa.com>
2019-05-28 16:36:32 +0800

    Centos 6 and installer doc

---
+++ install-readme.txt
@@ -0,0 +1,7 @@
+
+#CENTOS 7 Installer
+curl https://raw.githubusercontent.com/digitalreachinsight/python-remote-monitor/master/setupCentos7.sh | sudo bash
+
+#CENTOS 6 Installer
+curl https://raw.githubusercontent.com/digitalreachinsight/python-remote-monitor/master/setupCentos6.sh | sudo bash
+
---
+++ setupCentos6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yum install python36 python-pip git -y
+pip install -U pip
+pip install -U virtualenv
+cd /usr/lib/
+git clone https://github.com/digitalreachinsight/python-remote-monitor.git 
+cd /usr/lib/python-remote-monitor/
+virtualenv -p python36 venv
+/usr/lib/python-remote-monitor/venv/bin/pip3 install -r requirements.txt 
+chmod +x /usr/lib/python-remote-monitor/update.sh
+groupadd drmonitor
+useradd -m drmonitor -g drmonitor -s /usr/sbin/nologin
+chown -R drmonitor.drmonitor /usr/lib/python-remote-monitor/
